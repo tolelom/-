879fdce5a99eea2b7ea3bf9cf2b6952d90bf1572
tolelom <98kimsungmin@naver.com>
2018-10-19 22:26:52 +0900

    수정본

--- KiwiJuiceEasy.cpp
+++ KiwiJuiceEasy.cpp
@@ -7,18 +7,11 @@ class KiwiJuiceEasy {
 public:
 	vector <int> thePouring(vector <int> capacities, vector <int> bottles, vector <int> fromId, vector <int> toId) {
 		for (int i = 0; i < fromId.size; i++) {
-			int f = fromId[i];
-			int t = toId[i];
-			int space = capacities[t] - bottles[t];
-			if (space >= bottles[f]) { //이상 없을 때  
-				bottles[t] += bottles[f];
-				bottles[f] = 0;
-			}
-			else {//문제 있을 때 (넘침) 해서 해결하면 될 듯 
-				bottles[t] += space;
-				bottles[f] -= space;
-			}
+			int sum = bottles[fromId[i]] + bottles[toId[i]];
+			bottles[toId[i]] = min(sum, capacities[toId[i]]);
+			bottles[fromId[i]] = sum - bottles[toId[i]];
 		}
+
 		return bottles;
 	}
 };
